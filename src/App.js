fed7387d17d57b0fa4ecc6def015ccd5f5f409e1
안병훈 <82787408+dhshffnfn123@users.noreply.github.com>
2021-11-13 23:08:20 +0900

    파라미터를 이용한 카테고리 선택

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import React, { useState, useCallback } from "react";
-import NewsList from "./components/NewsList";
-import Categories from "./components/Categories";
+import React from "react";
+import { Route } from 'react-router-dom';
+import NewsPage from "./pages/NewsPage";
 
 const App = () => {
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback(category => setCategory(category), []);
-
-  return (
-    <>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-    </>
-  );
+  return <Route path="/:category?" component={NewsPage} />;
+  // /:category 뒤에 물음표는 선택적이라는 뜻(있을수도 있고 없을수도 있다)이다.
 };
 
 export default App;
