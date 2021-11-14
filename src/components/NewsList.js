d49458f96296612a5d9fed43ca4736770957e597
B H An <anbyunghoon0107@gamil.com>
2021-11-14 15:19:42 +0900

    usePromise를 활용한 코드 간소화

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import NewsItem from "./NewsItem";
 import axios from "axios";
+import usePromise from "../lib/usePromise";
 
 const NewsListBlock = styled.div`
     box-sizing: border-box;
@@ -25,38 +26,28 @@ const sampleArticle = {
 };
 
 const NewsList = ({ category }) => {
-    const [articles, setArticles] = useState(null);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-        // async를 사용하는 함수 선언
-        const fetchData = async () => {
-            setLoading(true);
-            try {
-                const query = category === 'all' ? '' : `&category=${category}`;
-                const response = await axios.get(
-                    `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=b5aee5a2926440d0a34450fa1ba10ae4`,
-                );
-                setArticles(response.data.articles);
-            } catch(e) {
-                console.log(e);
-            }
-            setLoading(false);
-        };
-        fetchData();
-    },[category]);
+    const [loading, response, error] = usePromise(() => {
+        const query = category === 'all' ? '' : `&category=${category}`;
+        return axios.get(
+            `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=b5aee5a2926440d0a34450fa1ba10ae4`,
+        );
+    }, [category]);
 
     // 대기 중
     if (loading) {
         return <NewsListBlock>대기 중...</NewsListBlock>;
     }
     // 아직 articles 값이 설정되지 않았을 때
-    if (!articles) {
+    if (!response) {
         return null;
     }
+    // 에러가 발생했을 때
+    if (error) {
+        return <NewsListBlock>에러 발생!</NewsListBlock>;
+    }
 
-    //articles 값이 유효할 때
-
+    // response 값이 유효할 때
+    const { articles } = response.data;
     return ( 
         <NewsListBlock>
             {articles.map(article => (
---
+++ src/lib/usePromise.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import { resolve } from '../../node_modules/jest-config/build/utils';
+
+export default function usePromise(promiseCreator, deps) {
+    // 대기 중/완료/실패에 대한 상태 관리
+    const [loading, setLoading] = useState(false);
+    const [resolved, setResolved] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const process = async () => {
+            setLoading(true);
+            try {
+                const resolved = await promiseCreator();
+                setResolved(resolve);
+            } catch (e) {
+                setError(e);
+            }
+            setLoading(false);
+        };
+        process();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, deps);
+
+    return [loading, resolved, error];
+}
